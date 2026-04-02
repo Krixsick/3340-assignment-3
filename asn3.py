d5166c472fc72da319f88025e98610bd3419d3ce
Krixsick <linusgao123@gmail.com>
2026-04-01 21:53:59 -0400

    Structure of code

---
+++ asn3.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+class Heap:
+    
+    def __init__(self):
+        pass
+    def in_heap(id):
+        pass
+    def is_empty():
+        pass
+    def min_key():
+        pass
+    def min_id():
+        pass
+    def key(id):
+        pass
+    def delete_min():
+        pass
+    def decrease_key(id, new_key):
+        pass
